1fa9867f7b74a08d131fd3a8727b3ebe96b2db9f
NetworkArchetype <networkarchetype@gmail.com>
2026-01-14 02:35:48 -0800

    CI: make quantum-harness shots/iters configurable

--- Code_core_3/plm-formalized/plm_formalized/__init__.py
+++ Code_core_3/plm-formalized/plm_formalized/__init__.py
@@ -2,7 +2,7 @@ from .model import PLMInputs, plm_ratio, plm_secret_value, hex_to_int
 from .stateful import PLMState, StatefulPLM
 from .quantum_temporal import QuantumTemporalConfig, simulate_time_series
 
-__version__ = "0.1.11"
+__version__ = "0.1.12"
 
 __all__ = [
     "PLMInputs",
